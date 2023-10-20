61ee40b01757c427086f45f3e50d69b9d12eac00
murrelvenlo <r0781309@student.thomasmore.be>
2023-10-20 12:01:45 +0200

    Fixing connection 2.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 const uri = process.env.MONGO_URI
 console.log("Mongo URI:", uri); // Log the URI
 
-mongoose.connect(uri)
+mongoose.connect(uri, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
